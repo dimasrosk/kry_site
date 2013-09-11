d2ff90a7bb3b3782742b0dc5e17ccf5acc4b1ae7
Dmytro Skrypnyk <dimasrosk@gmail.com>
2013-09-11 19:02:25 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KrySite::Application.config.secret_token = '193171bb740751e599f3424b2079a3d2eeae1e7090414a8c5f2e2285735814e8a12712e00e12b4f5e2d0050537011c4ace4ad39bd7249ad6659b66df87f272fc'
